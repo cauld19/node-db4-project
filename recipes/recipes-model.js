e4b611b3937849e2c8c1eda0b522e55be5f95a98
cauld19 <50849710+cauld19@users.noreply.github.com>
2020-02-06 22:41:15 -0800

    Fetch working for ingredients in recipes

--- recipes/recipes-model.js
+++ recipes/recipes-model.js
@@ -2,8 +2,8 @@ const db = require('../data/db-config.js');
 
 module.exports = {
     find,
-    // add,
-    // findById,
+    getShoppingList,
+    findById,
     // findSteps,
     // remove,
     // update,
@@ -13,3 +13,20 @@ module.exports = {
 function find() {
     return db('recipes')
 }
+
+function findById(id) {
+    return db('recipes')
+      .where({ id })
+      .first();
+};
+
+
+// getShoppingList(recipe_id)`: should return a list of all ingredients and quantities for a given recipe
+
+function getShoppingList(recipe_id) {
+    return db("recipes_ingredients as ri")
+      .join( "ingredients as i", "ri.ingredient_id", "i.id")
+      .join("recipes as r", "ri.recipe_id", "r.id")
+      .select("r.recipe_name", "i.ingredient_name", "i.ingredient_amount_cups", "i.ingredient_amount_oz", "i.ingredient_amount_grams")
+      .where({ recipe_id });
+  }
--- recipes/recipes-router.js
+++ recipes/recipes-router.js
@@ -15,6 +15,40 @@ router.get('/', (req, res) => {
   });
 
 
+  router.get('/:id/ingredients', (req, res) => {
+    const { id } = req.params;
+    console.log(id)
+  
+    Recipes.getShoppingList(id)
+    .then(ingredients => {
+      if (ingredients.length) {
+        res.json(ingredients);
+      } else {
+        res.status(404).json({ message: 'Could not find ingredients for given recipe' })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Failed to get ingredients' });
+    });
+  });
+
+  
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+
+  Recipes.findById(id)
+  .then(recipe => {
+    if (recipe) {
+      res.json(recipe);
+    } else {
+      res.status(404).json({ message: 'Could not find recipe with given id.' })
+    }
+  })
+  .catch(err => {
+    res.status(500).json({ message: 'Failed to get recipes' });
+  });
+});
+
 
 
 
